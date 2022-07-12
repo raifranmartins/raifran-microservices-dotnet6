fbed1dd1f425cf684e12a2cca932bc202423b07c
raifranmartins <raifranbsb@hotmail.com>
2022-07-12 16:39:58 -0300

    organazing

---
+++ S08-Microservices.Net6-OrganizingFirstMicroservicesArchitecture/GeekShooping/GeekShopping.Api/Model/Product.cs
@@ -0,0 +1,36 @@
+﻿using GeekShopping.Api.Model.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.Api.Model
+{
+
+    [Table("product")]
+    public class Product : BaseEntity
+    {
+        [Column("name")]
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; }
+
+        [Column("price")]
+        [Required]
+        [Range(1,10000)]
+        public decimal Price { get; set; }
+
+        [Column("description")]        
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Column("category_name")]
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        [Column("iamge_url")]
+        [StringLength(300)]
+        public string ImageURL { get; set; }
+
+
+
+    }
+}
