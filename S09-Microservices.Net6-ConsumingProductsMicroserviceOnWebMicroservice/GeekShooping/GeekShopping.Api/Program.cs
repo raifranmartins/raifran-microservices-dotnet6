65fd649f5f6d24fb0c3b99029a1556b395f45918
raifranmartins <raifranbsb@hotmail.com>
2022-07-12 16:41:06 -0300

    Consuming

---
+++ S09-Microservices.Net6-ConsumingProductsMicroserviceOnWebMicroservice/GeekShooping/GeekShopping.Api/Program.cs
@@ -0,0 +1,42 @@
+using AutoMapper;
+using GeekShopping.Api.Config;
+using GeekShopping.Api.Model.Context;
+using GeekShopping.Api.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options
+    .UseMySql(connection,
+        new MySqlServerVersion(
+            new Version(8, 0, 5))));
+
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
